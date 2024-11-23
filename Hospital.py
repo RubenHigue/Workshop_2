6b1eef8cafb6513e583102a53ceceb3708cf3792
RubenHigue <rubenhiguerasgutierrez@gmail.com>
2024-11-23 12:44:57 +0200

    Patient life cycle implemented.

--- Hospital.py
+++ Hospital.py
@@ -6,6 +6,7 @@ import simpy
 import random
 
 ONE = 1
+ZERO = 0
 
 
 class Hospital:
@@ -18,3 +19,29 @@ class Hospital:
         self.total_patients = 0
         self.blocked_surgeries = 0
         self.departed_patients = 0
+
+    def patient_life_time(self, patient):
+        arrival_time = self.env.now
+
+        patient.status = "Preparing"
+        with self.preparationRooms.resource.request(priority=patient.priority) as request:
+            yield request
+            yield self.env.timeout(patient.service_times["preparation"])
+
+        patient.status = "Surgery"
+        with self.surgery.resource.request(priority=patient.priority) as request:
+            if len(self.surgery.resource.queue) > ZERO:
+                self.blocked_surgeries += ONE
+            yield request
+            yield self.env.timeout(patient.service_times["surgery"])
+
+        patient.status = "Recovery"
+        with self.recovery.resource.request(priority=patient.priority) as request:
+            yield request
+            yield self.env.timeout(patient.service_times["recovery"])
+
+        patient.status = "Departed"
+        patient.total_time = self.env.now - arrival_time
+        self.departed_patients += ONE
+
+
